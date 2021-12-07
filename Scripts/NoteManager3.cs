823337693e9fcfbe1b40f71fb18e743d63ef1b57
NomAyuG19022 <hj-g19022@sist.ac.jp>
2021-12-07 11:46:28 +0900

    判定とノーツ関連のスクリプト追加（中身はほぼ同じで座標云々が変わるだけ）

---
+++ Scripts/NoteManager3.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class NoteManager3 : MonoBehaviour
+{
+    public GameObject notePrefab;
+    bool noteInstNow;//ノーツが作れるかどうか
+
+    private int cnt;
+    private int note_cnt;//ノーツ総数
+    // Start is called before the first frame update
+    void Start()
+    {
+        noteInstNow = true;
+        cnt = 0;
+        note_cnt = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.frameCount % 12 == 0 && noteInstNow == true)
+        {
+            SpawnNote();
+        }
+
+    }
+
+    public void SpawnNote()
+    {
+
+        if (noteInstNow == true)
+        {
+            Instantiate(notePrefab, new Vector3(5, 10, 0), Quaternion.identity);//ノーツ生成
+            cnt++;
+            note_cnt++;
+        }
+
+
+        if (cnt > 0)
+        {
+            noteInstNow = false;
+            cnt++;
+        }
+        if (Time.frameCount % 12 == 0)
+        {
+            cnt = 0;
+            noteInstNow = true;
+        }
+
+    }
+}
+//ノーツ生成修正すべきこと
+//・1フレーム単位での生成だと処理落ちしたときに抜けちゃう
+//10フレームに1回の範囲で生成したけどやっぱある程度抜けるね…
+
+
---
+++ Scripts/Note_Left.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Note_Left : MonoBehaviour
+{
+    private float moveSpeed = -7.5f;
+
+    private GameObject gameManager;
+    private GameObject judgePrefab;
+    public float moveX;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManager = GameObject.Find("GameManager");
+        judgePrefab = GameObject.Find("Judge");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(((moveSpeed / moveX) * Time.deltaTime), moveSpeed * Time.deltaTime, 0);//ノーツの移動
+
+    }
+    void OnTriggerExit2D(Collider2D coll)//判定はキーを押したときのみ表示される
+    {
+
+        if (coll.CompareTag("Delete"))//ジャッジエリアから外れたら
+        {
+            gameManager.GetComponent<GameManager>().judgeKind(5);
+            gameManager.GetComponent<GameManager>().ComboKill();
+            Destroy(gameObject);
+        }
+
+
+    }
+
+}
+//ノーツ１つに付けるべきもの
+/*
+ * ノーツそれぞれにキー入力の種類を持たせる
+ * 
+ キーを押す→ノーツと判定の位置を取る→判定ごとの処理をする
+ */
