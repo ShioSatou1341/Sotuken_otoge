c6ceeb78321f6b55ee102646bbd2afaf827241af
NomAyuG19022 <hj-g19022@sist.ac.jp>
2021-12-10 13:36:02 +0900

    ノーツ生成のスクリプト修正。これでとりあえず終わり

--- Assets/Scripts/Note/NoteManager.cs
+++ Assets/Scripts/Note/NoteManager.cs
@@ -7,58 +7,23 @@ using UnityEngine.SceneManagement;
 public class NoteManager : MonoBehaviour
 {
     public GameObject notePrefab;
-    bool noteInstNow;//ノーツが作れるかどうか
 
-    private int cnt;
-    //private int note_cnt;//ノーツ総数
     // Start is called before the first frame update
     void Start()
     {
-        noteInstNow = true;
-        cnt = 0;
-        //note_cnt = 0;
+
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        //SpawnNote();
-
-        if (Time.frameCount % 30 == 0 /*&& noteInstNow == true*/)//updateが60回/秒で動くからその半分の早さで処理させる
-        {
-            Instantiate(notePrefab, new Vector3(0, 10, 0), Quaternion.identity);//ノーツ生成s
-            Debug.Log("SpawnNote");
-            noteInstNow = false;
-        }//まだ抜けができるし、判定自体もダメ。でもこれで生成自体は幾分マシになった
     }
 
-    public void SpawnNote()
+    public void SpawnNote()//TLでマーカーを置くことでそのタイミングで生成できる
     {
-
-        
-        //if (noteInstNow == true)
-        //{
-        //    Instantiate(notePrefab, new Vector3(0, 10, 0), Quaternion.identity);//ノーツ生成s
-        //    cnt++;
-        //    //note_cnt++;
-        //    Debug.Log("SpawnNote");
-        //}
-
-
-        //if (cnt > 0)
-        //{
-        //    noteInstNow = false;
-        //    cnt++;
-        //}
-        //if (Time.frameCount % 12 == 0)
-        //{
-        //    cnt = 0;
-        //    noteInstNow = true;
-        //}
+        Instantiate(notePrefab, new Vector3(0, 10, 0), Quaternion.identity);//ノーツ生成s
 
     }
+
 }
-//ノーツ生成修正すべきこと
-//・1フレーム単位での生成だと処理落ちしたときに抜けちゃう
-//でも2フレームとるなりすると重複する
--- Assets/Scripts/Note/NoteManager2.cs
+++ Assets/Scripts/Note/NoteManager2.cs
@@ -7,33 +7,23 @@ using UnityEngine.SceneManagement;
 public class NoteManager2 : MonoBehaviour
 {
     public GameObject notePrefab;
-    bool noteInstNow;//ノーツが作れるかどうか
 
-    private int cnt;
-    //private int note_cnt;//ノーツ総数
     // Start is called before the first frame update
     void Start()
     {
-        noteInstNow = true;
-        cnt = 0;
-        //note_cnt = 0;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Time.frameCount % 30 == 0 /*&& noteInstNow == true*/)
-        {
-            Instantiate(notePrefab, new Vector3(-5, 10, 0), Quaternion.identity);//ノーツ生成s
-            Debug.Log("SpawnNote");
-            noteInstNow = false;
-        }
+
+    }
+
+    public void SpawnNote()//TLでマーカーを置くことでそのタイミングで生成できる
+    {
+        Instantiate(notePrefab, new Vector3(-5, 10, 0), Quaternion.identity);//ノーツ生成s
 
     }
 
 }
-//ノーツ生成修正すべきこと
-//・1フレーム単位での生成だと処理落ちしたときに抜けちゃう
-//10フレームに1回の範囲で生成したけどやっぱある程度抜けるね…
-
-
