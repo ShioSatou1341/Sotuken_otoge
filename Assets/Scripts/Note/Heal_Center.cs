7e272de93ff8a0a430332248a34ff2199cdb7e8b
NomAyuG19022 <hj-g19022@sist.ac.jp>
2022-01-19 14:27:18 +0900

    CureNoteをHeal_Centerに名前変更

--- Assets/Scripts/Note/Heal_Center.cs
+++ Assets/Scripts/Note/Heal_Center.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class CureNote : MonoBehaviour
+public class Heal_Center : MonoBehaviour
 {
     private float moveSpeed = -7.5f;
 
